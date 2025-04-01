aec5261b8d77caad3d6046aeba017835f7a5f125
DimaITDeveloper <dima.shiriev1@GMAIL.COM>
2025-04-01 22:26:03 +0700

    1

--- Assets/LearnMaterials 2/Scripts/ScalerModule.cs
+++ Assets/LearnMaterials 2/Scripts/ScalerModule.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using UnityEngine;
 
+
 [HelpURL("https://docs.google.com/document/d/1rdTEVSrCcYOjqTJcFCHj46RvnbdJhmQUb3gHMDhVftI/edit?usp=sharing")]
 public class ScalerModule : MonoBehaviour
 {
-    private Vector3 targetScale = new Vector3(2,2,2);
+    [Header("Scale Settings")]
+    [Tooltip("Target scale when activated")]
+    [Min(0.1f)] // Ограничение минимального значения
+    public Vector3 targetScale = new Vector3(2, 2, 2);
 
-    private float changeSpeed;
+    [Tooltip("Default scale of the object")]
+    [Min(0.1f)]
+    public Vector3 defaultScale = Vector3.one; // Делаем настраиваемым из инспектора
 
-    private Vector3 defaultScale;
+    [Space(10)]
+    [Tooltip("Speed of scaling animation")]
+    [Min(0.01f)]
+    public float changeSpeed = 1f;
+
+    [Header("Debug")]
+    [SerializeField]
+    private bool toDefault = false;
+
     private Transform myTransform;
-    private bool toDefault;
 
-    private void Start()
+    private void Awake()
     {
         myTransform = transform;
-        defaultScale = myTransform.localScale;
-        toDefault = false;
+        // Сохраняем начальный scale, если defaultScale не был изменен в инспекторе
+        if (defaultScale == Vector3.one)
+        {
+            defaultScale = myTransform.localScale;
+        }
     }
 
+    // Метод для вызова из редактора
+    [ContextMenu("Activate Module")]
     public void ActivateModule()
     {
         Vector3 target = toDefault ? defaultScale : targetScale;
@@ -27,6 +45,8 @@ public class ScalerModule : MonoBehaviour
         toDefault = !toDefault;
     }
 
+    // Метод для вызова из редактора
+    [ContextMenu("Return to Default")]
     public void ReturnToDefaultState()
     {
         toDefault = true;
@@ -37,12 +57,26 @@ public class ScalerModule : MonoBehaviour
     {
         Vector3 start = myTransform.lossyScale;
         float t = 0;
-        while(t < 1)
+        while (t < 1)
         {
             t += Time.deltaTime * changeSpeed;
             myTransform.localScale = Vector3.Lerp(start, target, t);
             yield return null;
         }
         myTransform.localScale = target;
+    }
+
+    // Добавляем метод для задания из задания (возможно опечатка в ActionMethodule)
+    public void ActionMethodule()
+    {
+        ActivateModule(); // Или другая логика, если требуется
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // Нажатие пробела
+        {
+            ActivateModule();
+        }
     }
 }
