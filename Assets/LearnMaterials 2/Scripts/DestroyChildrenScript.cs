20b4d1b29b9468490f9c4d46e04d15a0b781dc47
mxs9i <m.abora@mail.ru>
2025-04-02 22:32:42 +0700

    Destroy Children Script

---
+++ Assets/LearnMaterials 2/Scripts/DestroyChildrenScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SampleScript : MonoBehaviour
+{
+    public abstract void Use();
+}
+
+public class SampleScriptController : MonoBehaviour
+{
+    public List<SampleScript> scripts;
+
+    public void ActivateAll()
+    {
+        foreach (var script in scripts)
+        {
+            script.Use();
+        }
+    }
+}
+
+public class DestroyChildrenScript : SampleScript
+{
+    public Transform target;
+    public float shrinkDuration = 1f;
+
+    public override void Use()
+    {
+        StartCoroutine(DestroyChildrenWithShrink());
+    }
+
+    private IEnumerator DestroyChildrenWithShrink()
+    {
+        List<Transform> children = new List<Transform>();
+        foreach (Transform child in target)
+        {
+            children.Add(child);
+        }
+
+        float elapsedTime = 0;
+        while (elapsedTime < shrinkDuration)
+        {
+            foreach (Transform child in children)
+            {
+                if (child != null)
+                {
+                    child.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, elapsedTime / shrinkDuration);
+                }
+            }
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        foreach (Transform child in children)
+        {
+            if (child != null)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
